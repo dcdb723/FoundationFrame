63a8feb476daf7d1d324c60f7020c8a79adf9760
Victor <iceage723@gmail.com>
2017-11-20 16:15:21 +0800

    Initial Commit

---
+++ FoundationFrame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FoundationFrame
+//
+//  Created by Cong Deng on 2017/11/20.
+//  Copyright © 2017年 Cong Deng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
